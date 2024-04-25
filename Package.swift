6dd693301ba6517644b3465ae0523b822bb82afc
Cole Moore <Cartisim@icloud.com>
2024-04-25 15:55:47 +0800

    Update swift tools

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,8 +16,8 @@ let package = Package(
             targets: ["SwiftDTF"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.58.0")),
-        .package(url: "https://github.com/needle-tail/needletail-media-kit.git", branch: "main")
+        .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.65.0")),
+        .package(url: "git@github.com:needle-tail/needletail-media-kit.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
