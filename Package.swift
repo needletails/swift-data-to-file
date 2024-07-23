5a3ae27c40afce6fa123a72d293ecbf7b339b2a3
Cole Moore <Cartisim@icloud.com>
2024-07-23 09:40:08 +0800

    Updated Packages

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.65.0")),
-        .package(url: "git@github.com:needle-tail/needletail-media-kit.git", .upToNextMajor(from: "1.0.4")),
+        .package(url: "git@github.com:needle-tail/needletail-media-kit.git", .upToNextMajor(from: "1.0.5")),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
